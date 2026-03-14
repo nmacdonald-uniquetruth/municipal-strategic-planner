756d4989a41bc05b7d4f17564ddca33fbd5f6240
Nick MacDonald <financedirector@machiasme.gov>
2026-03-14 01:28:30 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import { ModelProvider } from './components/machias/ModelContext';
 
 export default function Layout({ children }) {
   return (
